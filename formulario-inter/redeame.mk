5a4d7104d05d20b71d0a96d0cafe52b082fa412c
Elaine Leandro <elainerleandro1@gmail.com>
2022-11-16 18:21:00 -0300

    Ajuste layout

---
+++ formulario-inter/redeame.mk
@@ -0,0 +1,8 @@
+<h1> Formulário Mentoria📝 </h1>
+
+Aplicação é um layout com contexto de um formulário, colocando em pratica o aprendizado semantica e as classes. 
+
+Conhecimento na linguagem
+☑️ 📄 HTML
+☑️ 👩🏻‍🎨 CSS
+
